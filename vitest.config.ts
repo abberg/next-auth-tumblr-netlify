a0592688457ab631ef9d7398d2055ffcdd077430
Andrew Berg <andrew@andrewberg.com>
2025-12-06 17:14:43 -0500

    ignore vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,9 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), storybookTest({
+    configDir: path.join(dirname, '.storybook')
+  })],
   test: {
     environment: 'jsdom',
     include: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
@@ -21,12 +23,6 @@ export default defineConfig({
     },
     projects: [{
       extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
       test: {
         name: 'storybook',
         browser: {
